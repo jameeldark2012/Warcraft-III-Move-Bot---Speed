bf2758c44040e3bea5ce51cf83ed4240fb48667c
Darkness Lore <125550741+jameeldark2012@users.noreply.github.com>
2024-09-10 17:20:11 +0300

    Added a fast screenshot to capture game state

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
 from environment import WarAMBOT
+import environment
+env =WarAMBOT()
+import keyboard_nofocus
+import time
+from PIL import Image
+#test = captureWindow_screenshot(environment.process_list[env.env_num].window_handle , 1024 , 1024)
+#time.sleep(10)
+# while True:
+#     keyboard_nofocus.press_key( "print_screen", environment.process_list[env.env_num].window_handle , duration= 0.1 , state="down" )
+#     keyboard_nofocus.press_key( "print_screen", environment.process_list[env.env_num].window_handle , duration= 0.1 , state="up" )
 
-WarAMBOT()
+
+import mss
+import mss.tools
+import win32gui
+
+
+
+
+# Example usage
+
 print()
